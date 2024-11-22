d686753b151e235a17e964e4b01c99cc5ee8fed0
Jarred Wilson <jarred.wilson12@gmail.com>
2024-11-22 18:04:09 +0000

    feat: add app provider

---
+++ lib/providers/application_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../backends/backend.dart';
+
+final appListProvider = Provider((ref) {return TestBackend().data.values.toList();});

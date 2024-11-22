9eb7c2f91dd8785572702c1ef38264599eb77f62
Jarred Wilson <jarred.wilson12@gmail.com>
2024-11-22 14:30:19 +0000

    feat: add page provider
    - add riverpod deps
    - wrap app in provider scope
    - add page notify provider
    - pull page info in navbar, sidebar, and view_box

---
+++ lib/providers/page_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+Map<String, Map<String, Object>> views = {
+      "bazaar": {
+            "widget": Container(),
+            "title": "Bazaar",
+        },
+      "play": {
+            "widget": Container(),
+            "title": "Play",
+        },
+      "work": {
+            "widget": Container(),
+            "title": "Work",
+        },
+      "build": {
+            "widget": Container(),
+            "title": "Build",
+        },
+      "socialize": {
+            "widget": Container(),
+            "title": "Socialize",
+        },
+      "relax": {
+            "widget": Container(),
+            "title": "Relax",
+        },
+      "device": {
+            "widget": Container(),
+            "title": "This Device",
+        },
+      "search": {
+            "widget": Container(),
+            "title": "Search",
+        },
+      "downloads": {
+            "widget": Container(),
+            "title": "Downloads",
+        },
+    };
+
+class PageNotifier extends Notifier<Map<String, Object>> {
+
+    @override
+    Map<String, Object> build() {
+        return views["bazaar"] ?? {};
+    }
+
+    void changePage(String page) {
+        if (views.containsKey(page)) {
+            state = views[page] ?? {};
+        }
+    }
+
+}
+
+
+final pageNotifierProvider = NotifierProvider<PageNotifier, Map<String, Object>>(() {return PageNotifier();});

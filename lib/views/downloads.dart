5b92cdc9caa3ec154cac51e5037101daa3cc5843
Jarred Wilson <jarred.wilson12@gmail.com>
2024-12-10 23:34:50 +0000

    feat: add downloads view

---
+++ lib/views/downloads.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class AppList extends StatelessWidget {
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            color: Colors.yellow,
+            height: 400,
+            width: 700,
+        );
+    }
+}

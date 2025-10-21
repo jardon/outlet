a5db728df4da048986624a2f1d1a51a520ad27cb
Jarred Wilson <jarred.wilson12@gmail.com>
2025-10-21 12:27:24 -0500

    refactor: rework TestBackend to use new Backend interface

--- lib/providers/application_provider.dart
+++ lib/providers/application_provider.dart
@@ -3,5 +3,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../backends/backend.dart';
 
 final appListProvider = Provider((ref) {
-  return TestBackend().data.values.toList();
+  return TestBackend().getInstalledPackages().values.toList();
 });

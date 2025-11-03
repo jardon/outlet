b1e6cc323d5410805a52ea1b025b8b924bfbcc31
Jarred Wilson <jarred.wilson12@gmail.com>
2025-11-03 13:31:39 -0500

    fix(screenshots): add black container behind screesnhot

--- lib/views/screenshots_view.dart
+++ lib/views/screenshots_view.dart
@@ -10,43 +10,45 @@ class ScreenshotsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Center(
-          child: Image.network(
-        screenshot,
-        fit: BoxFit.cover,
-        loadingBuilder: (context, child, loadingProgress) {
-          if (loadingProgress == null) return child;
-          return Center(
-            child: CircularProgressIndicator(
-              value: loadingProgress.expectedTotalBytes != null
-                  ? loadingProgress.cumulativeBytesLoaded /
-                      loadingProgress.expectedTotalBytes!
-                  : null,
-            ),
-          );
-        },
-        errorBuilder: (context, error, stackTrace) {
-          return const Text('Error loading image');
-        },
-      )),
-      Positioned(
-        top: 30,
-        left: 30,
-        child: Container(
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(45.0),
-            ),
-            child: CloseButton(
-              color: Colors.white,
-              onPressed: () {
-                if (Navigator.canPop(context)) {
-                  Navigator.of(context).pop();
-                }
-              },
-            )),
-      ),
-    ]);
+    return Container(
+        color: Colors.black,
+        child: Stack(children: [
+          Center(
+              child: Image.network(
+            screenshot,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              );
+            },
+            errorBuilder: (context, error, stackTrace) {
+              return const Text('Error loading image');
+            },
+          )),
+          Positioned(
+            top: 30,
+            left: 30,
+            child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(45.0),
+                ),
+                child: CloseButton(
+                  color: Colors.white,
+                  onPressed: () {
+                    if (Navigator.canPop(context)) {
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )),
+          ),
+        ]));
   }
 }

78c848e8f229250800d6059972d5490ab3f29dba
Jarred Wilson <jarred.wilson12@gmail.com>
2025-11-21 22:35:03 -0500

    feat: add basic AppActions component

---
+++ lib/views/components/app_actions.dart
@@ -0,0 +1,45 @@
+import '../../core/application.dart';
+import '../../providers/action_queue.dart';
+import '../../providers/backend_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AppActions extends ConsumerWidget {
+  final Application app;
+
+  const AppActions({
+    super.key,
+    required this.app,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final backend = ref.watch(backendProvider);
+    final actionQueue = ref.watch(actionQueueProvider);
+    return Container(
+      width: 400,
+      height: 70,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(45),
+          boxShadow: const [
+            BoxShadow(color: Colors.black12, blurRadius: 20.0)
+          ]),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Row(children: [
+          TextButton(
+            onPressed: () => actionQueue.add("installing ${app.id}", () async {
+              backend.installApplication(app.bundle!, "flathub");
+            }),
+            style: const ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll<Color>(Colors.black),
+            ),
+            child: Text(app.installed ? 'Uninstall' : 'Install',
+                style: const TextStyle(color: Colors.white)),
+          ),
+        ]),
+      ),
+    );
+  }
+}

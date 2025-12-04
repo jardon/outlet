8376de0d40e73c2f1dc1c5a355e7816b1a023bd5
Jarred Wilson <jarred.wilson12@gmail.com>
2025-12-04 07:08:34 -0600

    feat(appactions): watch liveApplicationProvider for changes

--- lib/views/components/app_actions.dart
+++ lib/views/components/app_actions.dart
@@ -1,5 +1,4 @@
 import '../../backends/backend.dart';
-import '../../core/application.dart';
 import '../../providers/action_queue.dart';
 import '../../providers/application_provider.dart';
 import 'dart:isolate';
@@ -7,55 +6,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AppActions extends ConsumerWidget {
-  final Application app;
+  final String id;
 
   const AppActions({
     super.key,
-    required this.app,
+    required this.id,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      width: 400,
-      height: 70,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(45),
-          boxShadow: const [
-            BoxShadow(color: Colors.black12, blurRadius: 20.0)
-          ]),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-        child: Row(spacing: 10, children: [
-          TextButton(
-            onPressed: () {
-              if (app.installed) {
-                final data = {"uninstallTarget": app.getUninstallTarget()};
-                _uninstallWorker(data);
-                ref.read(installedAppListProvider.notifier).refresh();
-              } else {
-                final data = {
-                  "installTarget": app.getInstallTarget(),
-                  "remote": app.remote!
-                };
-                ref.read(actionQueueProvider.notifier).add(
-                    "Installing ${app.name ?? app.id}", _installWorker, data);
-              }
-            },
-            style: const ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll<Color>(Colors.black),
+    final app = ref.watch(liveApplicationProvider(id));
+    return (app != null)
+        ? Container(
+            width: 400,
+            height: 70,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(45),
+                boxShadow: const [
+                  BoxShadow(color: Colors.black12, blurRadius: 20.0)
+                ]),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Row(spacing: 10, children: [
+                TextButton(
+                  onPressed: () {
+                    if (app.installed) {
+                      final data = {
+                        "uninstallTarget": app.getUninstallTarget()
+                      };
+                      _uninstallWorker(data);
+                      ref.read(installedAppListProvider.notifier).refresh();
+                    } else {
+                      final data = {
+                        "installTarget": app.getInstallTarget(),
+                        "remote": app.remote!
+                      };
+                      ref.read(actionQueueProvider.notifier).add(
+                          "Installing ${app.name ?? app.id}",
+                          _installWorker,
+                          data);
+                    }
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        WidgetStatePropertyAll<Color>(Colors.black),
+                  ),
+                  child: Text(app.installed ? 'Uninstall' : 'Install',
+                      style: const TextStyle(color: Colors.white)),
+                ),
+                Expanded(child: Container()),
+                Text(app.branch != null
+                    ? '${app.branch![0].toUpperCase()}${app.branch!.substring(1)}'
+                    : ''),
+              ]),
             ),
-            child: Text(app.installed ? 'Uninstall' : 'Install',
-                style: const TextStyle(color: Colors.white)),
-          ),
-          Expanded(child: Container()),
-          Text(app.branch != null
-              ? '${app.branch![0].toUpperCase()}${app.branch!.substring(1)}'
-              : ''),
-        ]),
-      ),
-    );
+          )
+        : Container();
   }
 }
 

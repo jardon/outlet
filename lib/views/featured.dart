3d4a16ebfdb1898b5a53ea2fc24354e2e71cd495
Jarred Wilson <jarred.wilson12@gmail.com>
2025-12-06 08:56:43 -0600

    feat(views): add category group views

---
+++ lib/views/featured.dart
@@ -0,0 +1,35 @@
+import '../providers/application_provider.dart';
+import 'components/app_list.dart';
+import 'components/categories_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Featured extends ConsumerWidget {
+  const Featured({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allApps = ref
+        .watch(appListProvider)
+        .values
+        .toList()
+        .where((app) => app.featured)
+        .toList();
+    return CustomScrollView(physics: const BouncingScrollPhysics(), slivers: [
+      SliverToBoxAdapter(
+          child: FeaturedCategoryCard(
+        apps: allApps.take(5).toList(),
+      )),
+      SliverToBoxAdapter(
+          child: SizedBox(
+              child: AppList(
+        apps: allApps.sublist(5),
+        scrollable: false,
+        rows: 5,
+        shrinkWrap: true,
+      ))),
+    ]);
+  }
+}
---
+++ lib/views/relax.dart
@@ -0,0 +1,44 @@
+import '../providers/application_provider.dart';
+import 'components/app_list.dart';
+import 'components/categories_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Relax extends ConsumerWidget {
+  const Relax({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final audioApps = ref.watch(appInCategoryList("audio"));
+    final videoApps = ref.watch(appInCategoryList("video"));
+    return CustomScrollView(physics: const BouncingScrollPhysics(), slivers: [
+      SliverToBoxAdapter(
+          child: VideoCategoryCard(
+        apps: videoApps.where((app) => app.featured).toList().take(5).toList(),
+      )),
+      SliverToBoxAdapter(
+          child: SizedBox(
+              child: AppList(
+        apps: videoApps,
+        scrollable: false,
+        rows: 2,
+        shrinkWrap: true,
+      ))),
+      const SliverToBoxAdapter(child: SizedBox(height: 10)),
+      SliverToBoxAdapter(
+          child: AudioCategoryCard(
+        apps: audioApps.where((app) => app.featured).toList().take(5).toList(),
+      )),
+      SliverToBoxAdapter(
+          child: SizedBox(
+              child: AppList(
+        apps: audioApps,
+        scrollable: false,
+        rows: 2,
+        shrinkWrap: true,
+      ))),
+    ]);
+  }
+}

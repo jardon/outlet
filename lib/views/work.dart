c39407e110b466c51dc457f48a6873d9d38fdb01
Jarred Wilson <jarred.wilson12@gmail.com>
2025-12-07 10:03:57 -0600

    ui(categorycards): add networking card

--- lib/views/work.dart
+++ lib/views/work.dart
@@ -12,6 +12,7 @@ class Work extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final gameApps = ref.watch(appInCategoryList("game"));
+    final networkApps = ref.watch(appInCategoryList("network"));
     return CustomScrollView(physics: const BouncingScrollPhysics(), slivers: [
       SliverToBoxAdapter(
           child: GraphicsCategoryCard(
@@ -21,6 +22,20 @@ class Work extends ConsumerWidget {
           child: SizedBox(
               child: AppList(
         apps: gameApps,
+        scrollable: false,
+        rows: 2,
+        shrinkWrap: true,
+      ))),
+      const SliverToBoxAdapter(child: SizedBox(height: 10)),
+      SliverToBoxAdapter(
+          child: NetworkCategoryCard(
+        apps:
+            networkApps.where((app) => app.featured).toList().take(5).toList(),
+      )),
+      SliverToBoxAdapter(
+          child: SizedBox(
+              child: AppList(
+        apps: networkApps,
         scrollable: false,
         rows: 2,
         shrinkWrap: true,

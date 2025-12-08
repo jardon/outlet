c5e79253c4d46292e145ffc94bc04f81044b6197
Jarred Wilson <jarred.wilson12@gmail.com>
2025-12-08 17:48:48 -0600

    feat(actionqueue): expose copy of queue with provider

--- lib/core/action_queue.dart
+++ lib/core/action_queue.dart
@@ -1,12 +1,14 @@
+import 'dart:collection';
+
 class ActionQueueStatus {
   final bool isIdle;
   final String? currentActionTitle;
-  final int queueLength;
+  final Queue<Action> queue;
 
   ActionQueueStatus({
     required this.isIdle,
     this.currentActionTitle,
-    required this.queueLength,
+    required this.queue,
   });
 
   @override
@@ -16,13 +18,11 @@ class ActionQueueStatus {
           runtimeType == other.runtimeType &&
           isIdle == other.isIdle &&
           currentActionTitle == other.currentActionTitle &&
-          queueLength == other.queueLength;
+          queue == other.queue;
 
   @override
   int get hashCode =>
-      isIdle.hashCode ^
-      (currentActionTitle?.hashCode ?? 0) ^
-      queueLength.hashCode;
+      isIdle.hashCode ^ (currentActionTitle?.hashCode ?? 0) ^ queue.hashCode;
 }
 
 class Action {

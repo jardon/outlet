1b1667b63f35d8a07cb4a2ab6d58d216e4ce8bc5
Jarred Wilson <jarred.wilson12@gmail.com>
2025-12-10 21:45:59 -0500

    feat[#49]: add search functionality
    - add fuzzywuzzy lib
    - utilize searchKeywordsProvider for gathering keywords
    - implement SearchAnchor and SearchBar for searching apps

--- lib/views/components/navbar.dart
+++ lib/views/components/navbar.dart
@@ -1,5 +1,11 @@
+import '../../providers/application_provider.dart';
+import '../app_view.dart';
+import '../navigation.dart';
+import 'app_icon_loader.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class Navbar extends StatelessWidget {
   const Navbar({
@@ -59,27 +65,8 @@ class Navbar extends StatelessWidget {
                     child: WindowTitleBarBox(child: MoveWindow()),
                   ),
                 ),
-                Container(
-                  width: 200,
-                  height: double.infinity,
-                  margin: const EdgeInsets.only(right: 40),
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(45.0),
-                      boxShadow: const [
-                        BoxShadow(color: Colors.black12, blurRadius: 15.0)
-                      ]),
-                  child: const Center(
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Search apps',
-                          icon: Icon(Icons.search),
-                          contentPadding: EdgeInsets.only(bottom: 10)),
-                    ),
-                  ),
-                ),
+                const SearchBarApp(),
+                const SizedBox(width: 40),
                 CloseWindowButton(
                     colors: WindowButtonColors(
                         mouseOver: const Color(0xFFD32F2F),
@@ -90,5 +77,90 @@ class Navbar extends StatelessWidget {
             ))
       ]),
     );
+  }
+}
+
+class SearchBarApp extends ConsumerStatefulWidget {
+  const SearchBarApp({super.key});
+
+  @override
+  ConsumerState<SearchBarApp> createState() => _SearchBarAppState();
+}
+
+class _SearchBarAppState extends ConsumerState<SearchBarApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 280,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25),
+            boxShadow: const [
+              BoxShadow(color: Colors.black12, blurRadius: 20.0)
+            ]),
+        child: SearchAnchor(
+          viewBackgroundColor: Colors.white,
+          builder: (BuildContext context, SearchController controller) {
+            return SearchBar(
+              controller: controller,
+              padding: const WidgetStatePropertyAll<EdgeInsets>(
+                EdgeInsets.symmetric(horizontal: 16.0),
+              ),
+              backgroundColor:
+                  const WidgetStatePropertyAll<Color>(Colors.white),
+              elevation: const WidgetStatePropertyAll<double>(0.0),
+              hintText: 'Search apps',
+              onTap: () {
+                controller.openView();
+              },
+              onChanged: (_) {
+                controller.openView();
+              },
+              leading: const Icon(Icons.search),
+            );
+          },
+          suggestionsBuilder:
+              (BuildContext context, SearchController controller) {
+            final searchKeywords = ref.read(searchKeywordsProvider);
+            final query = controller.text.toLowerCase();
+
+            final results = extractTop(
+              query: query,
+              choices: searchKeywords.keys.toList(),
+              limit: 5,
+              cutoff: 50,
+            );
+            return results.isNotEmpty
+                ? results.map((result) {
+                    final appId = searchKeywords[result.choice]!;
+                    final app = ref.read(liveApplicationProvider(appId));
+                    return ListTile(
+                      title: Text(app!.name ?? app.id),
+                      leading: SizedBox(
+                          width: 45, child: AppIconLoader(icon: app.icon)),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 8.0),
+                      onTap: () {
+                        controller.closeView(query);
+                        Navigator.push(
+                          context,
+                          NoAnimationPageRoute(
+                            pageBuilder: (context, animation1, animation2) =>
+                                Navigation(
+                              title: "App Info",
+                              child: AppView(app: app) as Widget,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }).toList()
+                : [
+                    const ListTile(
+                      title: Text("No results found."),
+                    )
+                  ];
+          },
+        ));
   }
 }

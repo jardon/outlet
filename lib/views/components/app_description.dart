45312bffc4be45a8d1f1907e5a3b9ddd9b6a7713
Jarred Wilson <jarred.wilson12@gmail.com>
2026-02-03 07:26:50 -0600

    refactor: move ExpandableContainer to separate file

--- lib/views/components/app_description.dart
+++ lib/views/components/app_description.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter/material.dart';
 import 'package:outlet/core/application.dart';
+import 'package:outlet/views/components/expandable_container.dart';
 import 'package:outlet/views/components/theme.dart';
 import 'package:outlet/views/components/badges.dart';
 
@@ -63,86 +64,6 @@ class AppDescription extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class ExpandableContainer extends StatefulWidget {
-  final Widget child;
-  final double maxHeight;
-
-  const ExpandableContainer({
-    super.key,
-    required this.child,
-    this.maxHeight = 150.0,
-  });
-
-  @override
-  State<ExpandableContainer> createState() => _ExpandableContainerState();
-}
-
-class _ExpandableContainerState extends State<ExpandableContainer> {
-  bool _isExpanded = false;
-  bool _showExpandButton = false;
-
-  final GlobalKey _contentKey = GlobalKey();
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance
-        .addPostFrameCallback((_) => _measureContentHeight());
-  }
-
-  void _measureContentHeight() {
-    final RenderBox? renderBox =
-        _contentKey.currentContext?.findRenderObject() as RenderBox?;
-
-    if (renderBox != null && renderBox.hasSize) {
-      final double actualHeight = renderBox.size.height;
-
-      if (actualHeight - widget.maxHeight > 20 && !_showExpandButton) {
-        setState(() {
-          _showExpandButton = true;
-        });
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final double maxConstraint =
-        _isExpanded || !_showExpandButton ? double.infinity : widget.maxHeight;
-
-    return Column(
-      children: [
-        ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: maxConstraint,
-          ),
-          child: KeyedSubtree(
-            key: _contentKey,
-            child: widget.child,
-          ),
-        ),
-        if (_showExpandButton)
-          Container(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {
-                  setState(() {
-                    _isExpanded = !_isExpanded;
-                  });
-                },
-                style: const ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.black),
-                ),
-                child: Text(
-                  _isExpanded ? 'Show Less' : 'Show More',
-                  style: const TextStyle(color: Colors.white),
-                ),
-              )),
-      ],
     );
   }
 }
